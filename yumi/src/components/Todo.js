b969740fb10d20b81112e57c6bc064bec5ce18d3
lazyduo <dadanuna@gmail.com>
2021-07-18 23:36:26 +0900

    add backdrop onCancel

--- yumi/src/components/Todo.js
+++ yumi/src/components/Todo.js
@@ -11,6 +11,10 @@ function Todo(props) {
         setModalIsOpen(true);
     }
 
+    function closeModalHandler() {
+        setModalIsOpen(false);
+    }
+
     return (
         <div className='card'>
             <h2>{props.text}</h2>
@@ -19,7 +23,7 @@ function Todo(props) {
             </div>
             {/* { modalIsOpen ? <Modal /> : null } */}
             { modalIsOpen && <Modal /> }
-            { modalIsOpen && <Backdrop /> }
+            { modalIsOpen && <Backdrop onCancel={closeModalHandler}/> }
         </div>
     );
 }
